413c95c27d2966e1e266ecb2bf8bab13309faf1f
Agzs <gzs_1994@163.com>
2017-08-11 09:13:09 +0800

    inline function test

---
+++ inlineFuncTest.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+   "fmt"
+   "math"
+)
+
+func main(){
+   /* 声明函数变量 */
+   getSquareRoot := func(x float64) float64 {
+      return math.Sqrt(x)
+   }
+
+   /* 使用函数 */
+   fmt.Println(getSquareRoot(9))
+   
+   fmt.Println(math.Sqrt(16))
+}
