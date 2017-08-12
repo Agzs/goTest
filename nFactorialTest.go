1b5ac49523e6b18d354da81c7f572a096a9b5681
Agzs <gzs_1994@163.com>
2017-08-12 08:56:40 +0800

    recursion function example

---
+++ nFactorialTest.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func Factorial(x int) int{
+  var result int
+  if x == 0 {
+    result = 1;	
+  } else {
+    result = x * Factorial(x - 1);
+  }
+  return result
+}
+
+func Factorial1(x int) (result int) {
+  if x == 0 {
+    result = 1;	
+  } else {
+    result = x * Factorial(x - 1);
+  }
+  return
+}
+
+func main() {  
+    var i int = 15
+    fmt.Printf("%d 的阶乘是 %d\n", i, Factorial(i))
+    fmt.Printf("%d 的阶乘是 %d\n", i, Factorial1(i))
+}
